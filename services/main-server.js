3ce0369745e0d461de8d613c43f508e38d21beaf
Shobana <shobanam@thoughtworks.com>
2013-12-29 02:36:09 +0530

    Added login and logout backend code using "passport" module.<Shobana>

--- services/main-server.js
+++ services/main-server.js
@@ -1,9 +1,12 @@
+var express = require('express')
+	, mongodb = require('mongodb').MongoClient
+	, fileUpload = require('fileupload')
+    , mkdirUtil = require('mkdirp')
+	, flash = require('connect-flash')
+	, findit = require('findit')
+	, passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
 
-var express = require('express');
-var findit = require('findit');
-var mongodb = require('mongodb').MongoClient;
-var fileUpload = require('fileupload');
-var mkdirUtil = require('mkdirp');
 var app = express();
 
 var properties = {
@@ -40,6 +43,85 @@ app.post('/category_structure/create.json', function(req, res) {
    });
  });	
 
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+	   findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      })
+  }
+));
+
+app.configure(function() {
+  app.use(express.static('public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(flash());
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+});
+
+app.get('/',function(req, res){
+	if(!req.user){
+		res.write("User is not logged in yet");
+	}
+	else{
+		res.write("Welcome" + " " + req.user.username);
+	}
+	res.end();
+});
+
+app.get('/login', function(req, res){
+   res.sendfile(__dirname+'/login.html');
+});
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
 app.get('/content/list.json', function (req, res) {
@@ -72,11 +154,9 @@ app.get('/content/list.json', function (req, res) {
 		path:"/images/IceCream.png"
 	}]);
 
-
 });
 
 app.get('/content/download',function (req, res) {
-	
 	var file = req.query.file || '';
 	res.download(file);
 
