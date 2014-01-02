49ce9f8aadda715e639afa0d455fb0378ea8914b
artherg <artherg@thoughtworks.com>
2014-01-02 19:50:32 +0530

    Arther - Pulled out the passport strategies outside the login method

--- services/main-server.js
+++ services/main-server.js
@@ -5,9 +5,78 @@ var express = require('express')
 , flash = require('connect-flash')
 , findit = require('findit')
 , passport = require('passport')
-, LocalStrategy = require('passport-local').Strategy;
+, localStrategy = require('passport-local').Strategy;
 
 var app = express();
+
+passport.use(new localStrategy(
+	function(username, password, done) {
+		findByUsername(username, function(err, user) {
+			if (err) { 
+				return done(err); 
+			}
+			if (!user) { 
+				return done(null, false, { message: 'Unknown user ' + username }); 
+			}
+			if (user.password != password) { 
+				return done(null, false, { message: 'Invalid password' }); 
+			}
+			return done(null, user);
+		})
+	}
+));
+
+var users = [{
+	id: 1,
+	username: 'bob',
+	password: 'secret',
+	email: 'bob@example.com'
+},{
+	id: 2,
+	username: 'joe',
+	password: 'birthday',
+	email: 'joe@example.com'
+}];
+
+var findById = function(id, fn) {
+	var idx = id - 1;
+	if (users[idx]) {
+		fn(null, users[idx]);
+	} else {
+		fn(new Error('User ' + id + ' does not exist'));
+	}
+}
+
+var findByUsername = function(username, fn) {
+	for (var i = 0, len = users.length; i < len; i++) {
+		var user = users[i];
+		if (user.username === username) {
+			return fn(null, user);
+		}
+	}
+	return fn(null, null);
+}
+
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	findById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+app.configure(function() {
+	app.use(express.static('public'));
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.session({ secret: 'keyboard cat' }));
+	app.use(flash());
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(app.router);
+});
 
 var properties = {
 	contentsFolder: "contents"
@@ -41,69 +110,6 @@ app.post('/category_structure/create.json', function(req, res) {
 			}
 		});
 	});	
-
-	var users = [{
-		id: 1,
-		username: 'bob',
-		password: 'secret',
-		email: 'bob@example.com'
-	},{
-		id: 2,
-		username: 'joe',
-		password: 'birthday',
-		email: 'joe@example.com'
-	}];
-
-	function findById(id, fn) {
-		var idx = id - 1;
-		if (users[idx]) {
-			fn(null, users[idx]);
-		} else {
-			fn(new Error('User ' + id + ' does not exist'));
-		}
-	}
-
-	function findByUsername(username, fn) {
-		for (var i = 0, len = users.length; i < len; i++) {
-			var user = users[i];
-			if (user.username === username) {
-				return fn(null, user);
-			}
-		}
-		return fn(null, null);
-	}
-
-	passport.serializeUser(function(user, done) {
-		done(null, user.id);
-	});
-
-	passport.deserializeUser(function(id, done) {
-		findById(id, function (err, user) {
-			done(err, user);
-		});
-	});
-
-	passport.use(new LocalStrategy(
-		function(username, password, done) {
-			findByUsername(username, function(err, user) {
-				if (err) { return done(err); }
-				if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-				if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-				return done(null, user);
-			})
-		}
-	));
-});
-
-app.configure(function() {
-	app.use(express.static('public'));
-	app.use(express.cookieParser());
-	app.use(express.bodyParser());
-	app.use(express.session({ secret: 'keyboard cat' }));
-	app.use(flash());
-	app.use(passport.initialize());
-	app.use(passport.session());
-	app.use(app.router);
 });
 
 app.get('/',function(req, res){
@@ -123,8 +129,9 @@ app.get('/login', function(req, res){
 app.post('/login',
 	passport.authenticate('local', { successRedirect: '/',
 		failureRedirect: '/login',
-		failureFlash: true })
-	);
+		failureFlash: true }
+	)
+);
 
 app.get('/logout', function(req, res){
 	req.logout();
@@ -193,4 +200,4 @@ app.post('/upload', function(req, res) {
 	});
 });
 
-app.listen(3000);
+app.listen(8080);
