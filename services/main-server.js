c49d19b062ef06a1a490a9b4f40b3f410b5f5e63
Ashok Gowtham M <ashokgo@thoughtworks.com>
2014-01-02 19:11:31 +0530

    Ashok | fixed indentations and errors

--- services/main-server.js
+++ services/main-server.js
@@ -1,11 +1,11 @@
 var express = require('express')
-	, mongodb = require('mongodb').MongoClient
-	, fileUpload = require('fileupload')
-    , mkdirUtil = require('mkdirp')
-	, flash = require('connect-flash')
-	, findit = require('findit')
-	, passport = require('passport')
-    , LocalStrategy = require('passport-local').Strategy;
+, mongodb = require('mongodb').MongoClient
+, fileUpload = require('fileupload')
+, mkdirUtil = require('mkdirp')
+, flash = require('connect-flash')
+, findit = require('findit')
+, passport = require('passport')
+, LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
@@ -14,89 +14,96 @@ var properties = {
 };
 
 var category_structure = {
-    location: "sci_fi/finding_neo_2"
+	location: "sci_fi/finding_neo_2"
 };
 
 app.post('/category_structure/create.json', function(req, res) {
-   mongodb.connect('mongodb://localhost:27017/content_locator', function(err, db) {
-   console.log("DB connected successfully" + db);
-   var location = category_structure.location; // to be replaced by req.body
-   console.log("Here " + location);
-   db.collection('content_locations').find({'location': new RegExp(location, 'i') }).toArray(function(err, folders) {
-   if(folders != null && folders.length > 0){
-   	    console.log(folders);
-        res.send(folders);
-        // res.status(409);
-    }
-    else{
-      db.collection('content_locations').insert(category_structure, function(err, contentLocation){
-		if(err){
-            console.log(err);
-            res.status(400);
+	mongodb.connect('mongodb://localhost:27017/content_locator', function(err, db) {
+		console.log("DB connected successfully" + db);
+		var location = category_structure.location; // to be replaced by req.body
+		console.log("Here " + location);
+		db.collection('content_locations').find({'location': new RegExp(location, 'i') }).toArray(function(err, folders) {
+			if(folders != null && folders.length > 0) {
+				console.log(folders);
+				res.send(folders);
+				// res.status(409);
+			}
+			else {
+				db.collection('content_locations').insert(category_structure, function(err, contentLocation){
+					if(err){
+						console.log(err);
+						res.status(400);
+					}else{
+						res.send("Successfully created a content category structure");
+						// res.status(201);
+					}
+				});
+			}
+		});
+	});	
 
-		}else{
-			res.send("Successfully created a content category structure");
-			// res.status(201);
+	var users = [{
+		id: 1,
+		username: 'bob',
+		password: 'secret',
+		email: 'bob@example.com'
+	},{
+		id: 2,
+		username: 'joe',
+		password: 'birthday',
+		email: 'joe@example.com'
+	}];
+
+	function findById(id, fn) {
+		var idx = id - 1;
+		if (users[idx]) {
+			fn(null, users[idx]);
+		} else {
+			fn(new Error('User ' + id + ' does not exist'));
 		}
-	  }); 
-    }
-   });
- });	
+	}
 
-var users = [
-    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
-  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
-];
+	function findByUsername(username, fn) {
+		for (var i = 0, len = users.length; i < len; i++) {
+			var user = users[i];
+			if (user.username === username) {
+				return fn(null, user);
+			}
+		}
+		return fn(null, null);
+	}
 
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
+	passport.serializeUser(function(user, done) {
+		done(null, user.id);
+	});
 
-function findByUsername(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
+	passport.deserializeUser(function(id, done) {
+		findById(id, function (err, user) {
+			done(err, user);
+		});
+	});
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
+	passport.use(new LocalStrategy(
+		function(username, password, done) {
+			findByUsername(username, function(err, user) {
+				if (err) { return done(err); }
+				if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+				if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+				return done(null, user);
+			})
+		}
+	));
 });
-
-passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-	   findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-        return done(null, user);
-      })
-  }
-));
 
 app.configure(function() {
-  app.use(express.static('public'));
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.session({ secret: 'keyboard cat' }));
-  app.use(flash());
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
+	app.use(express.static('public'));
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.session({ secret: 'keyboard cat' }));
+	app.use(flash());
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(app.router);
 });
 
 app.get('/',function(req, res){
@@ -110,18 +117,18 @@ app.get('/',function(req, res){
 });
 
 app.get('/login', function(req, res){
-   res.sendfile(__dirname+'/login.html');
+	res.sendfile(__dirname+'/login.html');
 });
 
 app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
-);
+	passport.authenticate('local', { successRedirect: '/',
+		failureRedirect: '/login',
+		failureFlash: true })
+	);
 
 app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+	req.logout();
+	res.redirect('/');
 });
 
 app.get('/content/list.json', function (req, res) {
@@ -163,26 +170,27 @@ app.get('/content/download',function (req, res) {
 });
 
 var requestFile = {
-  content : {
-  	path : "math/algebra",
-  	subjectMatter : "contents/algebra_one.txt"
-  } 
+	content : {
+		path : "math/algebra",
+		subjectMatter : "contents/algebra_one.txt"
+	} 
 };
 
 app.post('/upload', function(req, res) {
-	var subjectMatter =  requestFile.content.subjectMatter; //req.files.content.subjectMatter;
-	var promise = mkdirUtil("contents/"+ requestFile.content.path, function(err){
-     if(err) console.log("Error creating Directory Structure");
-     else {
-          	var fileUploader = fileUpload.createFileUpload("contents/"+ requestFile.content.path);
-   			fileUploader.put(subjectMatter,function(err, file){
-    		  if(err){
-    		  	console.log("Error captured during Upload Content " + err);
-    		  }
-    		  res.send(file);
-        	});
-    }
-   });
+	var subjectMatter = requestFile.content.subjectMatter;
+	var promise = mkdirUtil("contents/" + requestFile.content.path, function(err){
+		if(err) {
+			console.log("Error creating Directory Structure");
+		} else {
+			var fileUploader = fileUpload.createFileUpload("contents/"+ requestFile.content.path);
+			fileUploader.put(subjectMatter,function(err, file){
+				if(err) {
+					console.log("Error captured during Upload Content " + err);
+				}
+				res.send(file);
+			});
+		}
+	});
 });
 
 app.listen(3000);
